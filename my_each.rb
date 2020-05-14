59b179babbdcf178696dbb501834bc8a4ca391ca
Richard Cho <96richardohc@gmail.com>
2020-05-14 03:46:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(names) # put argument(s) here
   # code here
+  i = 0
+  while i < names.length
+    yield(names[i])
+    i = i + 1
+  end
+  names
 end
